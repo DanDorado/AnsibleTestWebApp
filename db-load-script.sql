278328511ecbaa9c9832fe7be957eb3ec191cd77
DanDorado <DanDorado@mail.com>
2020-12-01 09:31:16 +0000

    This commit was made through ansible gitpushpull.yml

--- db-load-script.sql
+++ db-load-script.sql
@@ -1,5 +1,5 @@
 USE ecomdb;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
 
-INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png");
+INSERT INTO products (Name) VALUES ("Laptop");
 
